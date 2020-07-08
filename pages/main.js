8bc37945ab9af065d2f7addcc3821a3119b38029
Aya Habeeb <aya.r.h98@gmail.com>
2020-07-08 15:49:45 +0300

    for testing

--- pages/main.js
+++ pages/main.js
@@ -1,9 +1,29 @@
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    //we ready baby
 
     const reqTable = document.querySelector('#requestTable');
+
+
+    async function sendPushNotification(expoPushToken) {
+        const message = {
+            to: expoPushToken,
+            sound: 'default',
+            title: 'Farha App',
+            body: 'تم قبول طلبك',
+
+        };
+
+        await fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Accept-encoding': 'gzip, deflate',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(message),
+        }).then(response => console.log(response.text()))
+    }
 
     try {
 
@@ -37,63 +57,82 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     approve.addEventListener('click',()=> {
                         let id = approve.getAttribute("id").slice(7);
                         //remove from requests + add to hall & room table
-                        firebase.firestore().collection('halls').add({
-                            name: data.hallName,
-                            address : data.hallAddress,
-                            owner : data.userId,
-                            roomNum : 1
-                        }).then((ref)=>{
-                            hallId = ref.id;
-                            hallAdded = true;
-                            console.log('hall added successfully',hallId);
+                        // firebase.firestore().collection('halls').add({
+                        //     name: data.hallName,
+                        //     address : data.hallAddress,
+                        //     owner : data.userId,
+                        //     roomNum : 1
+                        // }).then((ref)=>{
+                        //     hallId = ref.id;
+                        //     hallAdded = true;
+                        //     console.log('hall added successfully',hallId);
+                        //
+                        // });
+                        //
+                        // setTimeout(()=>{
+                        //     firebase.firestore().collection('rooms').add({
+                        //         name: data.roomName,
+                        //         numberOfPeople : data.roomPersons,
+                        //         hallId: hallId,
+                        //         price : data.roomPrice,
+                        //         services: data.serviceName ,
+                        //         servicePrice : data.servicePrice,
+                        //         address: data.hallAddress,
+                        //         description: data.hallDescription,
+                        //         roomNum : 1
+                        //     }).then(()=>{
+                        //         roomAdded = true;
+                        //         console.log('room added successfully')
+                        //     });
+                        // },4000);
 
-                        });
-
-                        setTimeout(()=>{
-                            firebase.firestore().collection('rooms').add({
-                                name: data.roomName,
-                                numberOfPeople : data.roomPersons,
-                                hallId: hallId,
-                                price : data.roomPrice,
-                                services: data.serviceName ,
-                                servicePrice : data.servicePrice,
-                                address: data.hallAddress,
-                                description: data.hallDescription,
-                                roomNum : 1
-                            }).then(()=>{
-                                roomAdded = true;
-                                console.log('room added successfully')
-                            });
-                        },4000);
+                        //
+                        // firebase.firestore().collection('users').doc(data.userId).update({
+                        //     manager: true,
+                        //     businessEmail : data.ownerEmail,
+                        //     businessPhone: data.ownerPhone
+                        // }).then(()=>{
+                        //     userUpdated = true;
+                        //     console.log('user updated')
+                        // });
 
 
-                        firebase.firestore().collection('users').doc(data.userId).update({
-                            manager: true,
-                            businessEmail : data.ownerEmail,
-                            businessPhone: data.ownerPhone
-                        }).then(()=>{
-                            userUpdated = true;
-                            console.log('user updated')
-                        });
 
+                        // setTimeout(()=> {
+                        //     try{
+                        //         firebase.firestore().collection('requests').doc(id).delete().then(function() {
+                        //             if (doc.exists) {
+                        //                 console.log("Document successfully deleted!");
+                        //             }else {
+                        //                 console.log('doc not exist')
+                        //             }
+                        //         }).catch(function(error) {
+                        //             console.error("Error removing document: ", error);
+                        //         });
+                        //     } catch (e) {
+                        //         console.log(e)
+                        //     }
+                        // },8000);
 
-                        setTimeout(()=> {
-                            try{
-                                firebase.firestore().collection('requests').doc(id).delete().then(function() {
-                                    if (doc.exists) {
-                                        console.log("Document successfully deleted!");
-                                    }else {
-                                        console.log('doc not exist')
-                                    }
-                                }).catch(function(error) {
-                                    console.error("Error removing document: ", error);
+                        // Create a reference to the cities collection
+                        let userInfo = firebase.firestore().collection("users");
+                        let query = userInfo.where("uid", "==", data.userId)
+                            .get()
+                            .then(function(querySnapshot) {
+                                querySnapshot.forEach(function(doc) {
+                                    // doc.data() is never undefined for query doc snapshots
+                                    console.log(doc.id, " => ", doc.data());
+                                    let expoToken = doc.data().expoPushToken;
+                                    console.log('token ' , doc.data().expoPushToken );
+                                    sendPushNotification(expoToken);
                                 });
-                            } catch (e) {
-                                console.log(e)
-                            }
-                        },5000);
+                            })
+                            .catch(function(error) {
+                                console.log("Error getting documents: ", error);
+                            });
 
-                        console.log(id);
+
+
                     });
 
 
